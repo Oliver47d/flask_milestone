5e98696ac440e87cc7169b346b4ab8d7296ff945
siqi_du <vagrant@s201806-41.localdomain>
2018-06-22 03:05:06 +0000

    first commit

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
 from flask import Flask, render_template, request, redirect
+import datetime as dt
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh.resources import CDN
+import quandl
+def datetime(x):
+    return np.array(x, dtype=np.datetime64)
 
-app = Flask(__name__)
-
-@app.route('/')
+@app.route('/index',methods=['GET','POST'])
 def index():
-  return render_template('index.html')
+  if request.method=="GET":
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
+  else:
+    ticker_sym=request.form['ticker']
+    price=request.form['features']
+    quandl.ApiConfig.api_key = 'uMFrusZD9H5V3e2uJ59A'
+    data = quandl.get_table('WIKI/PRICES', ticker = ticker_sym, qopts = { 'columns': ['date',price]}, date = { 'gte': '2017-05-10', 'lte': '2017-06-10' }, paginate=True)
+    p1 = figure(x_axis_type="datetime", title="Stock Prices")
+    p1.grid.grid_line_alpha=0.3
+    p1.xaxis.axis_label = 'Date'
+    p1.yaxis.axis_label = 'Price'
+
+    p1.line(data['date'], data[request.form['features']], color='#A6CEE3', legend=price)
+
+    script, div = components(p1)
+    cdn_js=CDN.js_files[0]
+    cdn_css=CDN.css_files[0]
+
+    return render_template('plot.html',ticker=ticker_sym,script=script,div=div,cdn_css=cdn_css,cdn_js=cdn_js)
+
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=0.0.0.0)
